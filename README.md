5b445dd5b0fa2b2b0e06f10620e44711b565f47e
Johnson Hsu <specialline4107@gmail.com>
2025-05-25 12:22:08 +0800

    Modify readme. Add create_file and clean_file to complete the job

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 "# ibm_module4_writefile" 
+
+Scenario to fulfill:
+Your local university's Raptors fan club maintains a register of its active members on a .txt document. Every month they update the file by removing the members who are not active. You have been tasked with automating this with your Python skills.
+Given the file currentMem, Remove each member with a 'no' in their Active column. Keep track of each of the removed members and append them to the exMem file. Make sure that the format of the original files in preserved. (Hint: Do this by reading/writing whole lines and ensuring the header remains )
+Run the code block below prior to starting the exercise. The skeleton code has been provided for you. Edit only the cleanFiles function.
+
+Step:
+1. Please run create_file.py to create 2 files (one is members.txt and another one is inactive.txt)
+2. Run clean.file.py to get result which satisfied the mission
---
+++ clean_file.py
@@ -0,0 +1,46 @@
+def cleanFiles(currentMem, exMem):
+    with open(currentMem, 'r+') as f_current:
+        whole_list = []
+        for line in f_current:
+            whole_list.append(line)
+        # print(whole_list)
+    
+    list_no_active = []
+    list_active = []
+    
+    for row in whole_list:
+        if 'Membership No' in row:
+            header = row
+    
+        elif 'no' in row:
+            list_no_active.append(row)
+        
+        elif 'yes' in row:
+            list_active.append(row)
+    
+    # print(header)
+    # print('list_no_active is:', list_no_active)
+    # print('list_active is:', list_active)
+
+    with open(exMem, 'a+') as f_noactive:
+        for row in list_no_active:
+            f_noactive.write(row)
+    
+    with open(currentMem, 'w') as f_active:
+        f_active.write(header)
+        for row in list_active:
+            f_active.write(row)
+
+cleanFiles('members.txt', 'inactive.txt')
+
+memReg = 'members.txt'
+exReg = 'inactive.txt'
+
+headers = "Membership No  Date Joined  Active  \n"
+with open(memReg,'r') as readFile:
+    print("Active Members: \n\n")
+    print(readFile.read())
+    
+with open(exReg,'r') as readFile:
+    print("Inactive Members: \n\n")
+    print(readFile.read())
